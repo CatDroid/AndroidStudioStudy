b8b669aa2b9219e3ae6b74d5953ad8464814fbd3
hanloong ho <1198432354@qq.com>
2016-12-26 08:40:00 +0800

    AS中引入jar包

--- app/src/main/java/com/example/rd0394/rtmpdemo/MainActivity.java
+++ app/src/main/java/com/example/rd0394/rtmpdemo/MainActivity.java
@@ -4,11 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.core.BQLVideoView;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    BQLVideoView mView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
